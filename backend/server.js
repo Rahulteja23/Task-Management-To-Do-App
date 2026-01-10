4d5e3470cb76653560279f3ed7b781f7e8354abf
Rahul Teja <teja63266@gmail.com>
2026-01-11 01:51:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("YOUR_MONGODB_URI")
+mongoose.connect("mongodb+srv://taskuser:admin@1234@rahul.u4npnf2.mongodb.net/?appName=Rahul")
 .then(() => console.log("MongoDB Connectserver.jsed"));
+jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+jwt.verify(token, process.env.JWT_SECRET)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
@@ -19,5 +21,6 @@ app.listen(5000, () => console.log("Server running on port 5000"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running"));
+
 
 
