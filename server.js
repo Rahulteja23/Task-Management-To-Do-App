232c52e96a0f6a11eda3c1edbfb02422dd8aa71c
Rahul Teja <teja63266@gmail.com>
2026-01-11 01:27:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("YOUR_MONGODB_URI")
+.then(() => console.log("MongoDB Connectserver.jsed"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
