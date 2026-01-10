6cbeebfa0948ec01f5186a09038528caf92acb3b
Rahul Teja <teja63266@gmail.com>
2026-01-11 01:30:34 +0530

    Create authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  const decoded = jwt.verify(token, "SECRET_KEY");
+  req.userId = decoded.id;
+  next();
+};
