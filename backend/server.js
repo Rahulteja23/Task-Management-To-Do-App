c51831cd93460bfaeb1058e3473c44ec6cd5dd26
Rahul Teja <teja63266@gmail.com>
2026-01-11 01:37:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,7 @@ app.use("/api/tasks", taskRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
+
+
