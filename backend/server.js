f135165b0d19acfc145a1af84c04a1a109d8f66f
Rahul Teja <teja63266@gmail.com>
2026-01-11 19:05:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://taskuser:admin@1234@rahul.u4npnf2.mongodb.net/?appName=Rahul")
+mongoose.connect<>
 .then(() => console.log("MongoDB Connectserver.jsed"));
 jwt.sign({ id: user._id }, process.env.JWT_SECRET)
 jwt.verify(token, process.env.JWT_SECRET)
